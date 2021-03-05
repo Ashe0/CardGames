667228962ed192ca0cd301592991186363965528
Asher Sonntag <asonntag2024@ravenscroft.org>
2021-03-05 18:39:29 +0000

    pseudocode for main menu, blackjack, go fish, and function for random deck

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
+import random
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+def makedeck():
+  deck = []
+  suits = ['S','C','H','D']
+  numbers = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
+  for i in range(len(suits)):
+    for q in range(len(numbers)):
+      card = numbers[q]+suits[i]
+      deck.append(card)
+  random.shuffle(deck)
+  return deck
+
+deck = makedeck()
+print(deck)
