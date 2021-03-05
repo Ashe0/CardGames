c305d42cf9caca5172446742eb9529e935f25bda
Asher Sonntag <asonntag2024@ravenscroft.org>
2021-03-05 19:54:01 +0000

    Added base for BlackJack ad GoFish games

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
 from cards import Cards
-blackjack = Cards('Blackjack',1)
-x = blackjack.draw()
-print(blackjack.translate(x))
 
 
+game_list = ["Black Jack","Go Fish!"]
+def main_menu():
+  #print games
+  for i in range(len(game_list)):
+    print(str(i+1)+": " + game_list[i])
+  #Get the game the player wants to play
+  choiceloop = True
+  while choiceloop:
+    choice = int(input("What number do you want to play?\n"))
+    #If number is invalid, ask again
+    if choice > len(game_list):
+      print("Invalid: Choose a valid number")
+    #If number is valid, do not ask again
+    else:
+      choiceloop = False
+  #Start the game the player picked
+  if choice == 1:
+    blackjack()
+  elif choice == 2:
+    gofish()
+  #elif choice == 3:
+
+
+def blackjack():
+  print("Blackjack")
+def gofish():
+  print("Go Fish")
+
+
+main_menu()
