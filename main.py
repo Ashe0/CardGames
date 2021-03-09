c1e14013170fe574b9b04758ccbb37a42b9b2fee
Asher Sonntag <asonntag2024@ravenscroft.org>
2021-03-09 15:40:01 +0000

    Finished the Blackjack Game [Hand Management, End States, Hand Score], added error message for typing letters

--- main.py
+++ main.py
@@ -2,35 +2,91 @@ from cards import Cards
 from player import Player
 
 you = Player("You")
-
+ai = Player("Ai")
 game_list = ["Black Jack","Go Fish!"]
 def main_menu():
   #print games
+  you.clear_hand()
+  ai.clear_hand()
   for i in range(len(game_list)):
     print(str(i+1)+": " + game_list[i])
   #Get the game the player wants to play
   choiceloop = True
   while choiceloop:
-    choice = int(input("What number do you want to play?\n"))
+    choice = input("What number do you want to play?\n")
     #If number is invalid, ask again
-    if choice > len(game_list):
-      print("Invalid: Choose a valid number")
-    #If number is valid, do not ask again
+    if choice.isdigit():
+      if int(choice) >= 3:
+        print("Invalid: Please choose a valid number")
+      else:
+        choiceloop = False
     else:
-      choiceloop = False
+        print("Invalid: Choose a valid number")
+      #If number is valid, do not ask again
   #Start the game the player picked
+  choice = int(choice)
   if choice == 1:
     blackjack()
   elif choice == 2:
     gofish()
-  #elif choice == 3:
+  
 
 
 def blackjack():
-  print("Blackjack")
+  blackjack = True
+  deck = Cards("Blackjack",2)
+  while blackjack:
+    you.draw(deck,2)
+    ai.draw(deck,2)
+    blackjackhands()
+    hitloop = True
+    while hitloop:
+      print(you.get_total())
+      choice = input("Hit(H) or Stay(S):\n")
+      if choice == "H":
+        you.draw(deck,1)
+      elif choice == "S":
+        hitloop = False
+        while ai.get_total() <= 16:
+          ai.draw(deck,1)
+          ai.show_hand()
+        if ai.get_total() > 21:
+          print("DEALER BUST: Player Wins")
+          main_menu()
+        elif you.get_total() == ai.get_total():
+          print("DRAW: Dealer Wins")
+          main_menu()
+        elif you.get_total() > ai.get_total():
+          print("WIN: Player Wins")
+          main_menu()
+        elif you.get_total() < ai.get_total():
+          print("LOSS: Dealer Wins")
+          main_menu()
+      else:
+        print("Enter a Valid Input")
+      blackjackhands()
+      if you.get_total() > 21:
+        print("BUST: Dealer Wins")
+        you.clear_hand()
+        ai.clear_hand()
+        main_menu()
+      
+
+      
+def blackjackhands():
+  print("YOUR HAND:")
   you.show_hand()
+  print("AI HAND:")
+  ai.show_card("XX")
+  for i in range(len(ai.hand)-1):
+    ai.show_card(ai.hand[1])
+
 def gofish():
-  print("Go Fish")
+  gofish = True
+  x=0
+  while gofish:
+    x+=1
+    print(x)
   
 
 
--- player.py
+++ player.py
@@ -1,17 +1,24 @@
 class Player:
   def __init__(self,name):
     self.name = name
-    self.hand = ["AD","3H","7S","10C"]
+    self.hand = []
   
-  def draw(self,deck):
-    self.hand.append(deck.draw())
+  def draw(self,deck,num):
+    for i in range(num):
+      self.hand.append(deck.draw())
+
+  def remove(self,x):
+    self.hand.remove(x)
+
+  def clear_hand(self):
+    self.hand = []
 
   def show_hand(self):
     for i in range(len(self.hand)):
       self.show_card(self.hand[i])
       
   def show_card(self,x):
-    print(" __________")
+    print(" _________")
     if len(x) == 3:
       print("|"+x[2]+"        |")
       print("|"+x[0:2]+"       |")
@@ -26,6 +33,27 @@ class Player:
     else:
       print("|        "+x[0]+"|")
       print("|________"+x[1]+"|")
+
+  def get_total(self):
+    total = 0
+    for i in range(len(self.hand)):
+      #If card is # 10
+      if self.hand[i][0:2] == "10":
+        total += int(self.hand[i][0:2])
+      #If card is Face
+      elif self.hand[i][0] == "K" or self.hand[i][0] == "Q" or self.hand[i][0] == "J":
+        total += 10
+      #If card is #
+      elif self.hand[i][0] == "2" or self.hand[i][0] =="3" or self.hand[i][0] == "4" or self.hand[i][0] == "5" or self.hand[i][0] == "6" or self.hand[i][0] == "7" or self.hand[i][0] == "8" or self.hand[i][0] == "9" or self.hand[i][0] =="10":
+        total += int(self.hand[i][0])
+      #If card is Ace and Ace Logic
+      elif self.hand[i][0] == "A":
+        if total+11 > 21:
+          total +=1
+        else:
+          total += 11
+    #Return Hand Total
+    return total
 
   def __eq__(self,obj):
     return self.name == obj.name
