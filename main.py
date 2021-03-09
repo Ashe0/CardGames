c9875daa72728ff3d2823f8ea6100398691e2eb1
Asher Sonntag <asonntag2024@ravenscroft.org>
2021-03-09 19:32:56 +0000

    Simplified BlackJack Game

--- main.py
+++ main.py
@@ -35,13 +35,14 @@ def main_menu():
 def blackjack():
   blackjack = True
   deck = Cards("Blackjack",2)
+  deck.makedeck()
   while blackjack:
     you.draw(deck,2)
     ai.draw(deck,2)
     blackjackhands()
     hitloop = True
+  
     while hitloop:
-      print(you.get_total())
       choice = input("Hit(H) or Stay(S):\n")
       if choice == "H":
         you.draw(deck,1)
@@ -49,29 +50,27 @@ def blackjack():
         hitloop = False
         while ai.get_total() <= 16:
           ai.draw(deck,1)
-          ai.show_hand()
+        print("-------------------")
+        ai.show_hand()
         if ai.get_total() > 21:
-          print("DEALER BUST: Player Wins")
-          main_menu()
+          game_end("DEALER BUST: Player Wins")
         elif you.get_total() == ai.get_total():
-          print("DRAW: Dealer Wins")
-          main_menu()
+          game_end("DRAW: Dealer Wins")
         elif you.get_total() > ai.get_total():
-          print("WIN: Player Wins")
-          main_menu()
+          game_end("WIN: Player Wins")
         elif you.get_total() < ai.get_total():
-          print("LOSS: Dealer Wins")
-          main_menu()
+          game_end("LOSS: Dealer Wins")
       else:
         print("Enter a Valid Input")
       blackjackhands()
       if you.get_total() > 21:
-        print("BUST: Dealer Wins")
-        you.clear_hand()
-        ai.clear_hand()
-        main_menu()
+        game_end("BUST: Dealer Wins")
       
-
+def game_end(text):
+  print(text)
+  you.clear_hand()
+  ai.clear_hand()
+  main_menu()
       
 def blackjackhands():
   print("YOUR HAND:")
@@ -80,6 +79,7 @@ def blackjackhands():
   ai.show_card("XX")
   for i in range(len(ai.hand)-1):
     ai.show_card(ai.hand[1])
+
 
 def gofish():
   gofish = True
